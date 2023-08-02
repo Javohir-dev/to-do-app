85679bc8af406665fcb3c6663381f0c81fccbefb
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-08-02 17:25:13 +0500

    added some models and forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import Tasks
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    tasks = Tasks.objects.all()
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {
+        "tasks": tasks,
+        "form": form,
+    }
+    return render(request, "tasks/list.html", context)
+
